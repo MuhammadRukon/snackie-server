cef2d46dbffc3366f1424fe06a37dd2d75417f24
MuhammadRukon <muhammad.rukon629@gmail.com>
2023-10-20 16:56:27 +0600

    all and specific user fetching api created

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 });
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.wgk6h9w.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://brandshop:2b0eMW6MQnxUEAvE@cluster0.wgk6h9w.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,9 +24,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const productCollection = client.db("productDB").collection("products");
     const brandCollection = client.db("productDB").collection("brands");
+    const userCollection = client.db("productDB").collection("users");
 
     //get all brands
     app.get("/brands", async (req, res) => {
@@ -50,6 +51,29 @@ async function run() {
       res.send(result);
     });
 
+    // get all users
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get specific user
+    app.get("/users/:email", async (req, res) => {
+      const paramEmail = req.params.email;
+      const query = { email: paramEmail };
+      const cursor = userCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get all products
+    app.get("/allproducts", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // get data according to brand name
     app.get("/products/:brandName", async (req, res) => {
       const brandName = req.params.brandName;
@@ -59,9 +83,9 @@ async function run() {
       res.send(result);
     });
 
+    //get specific product
     app.get("/details/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
       const cursor = productCollection.find(query);
       const result = await cursor.toArray();
@@ -75,7 +99,65 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // update product
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateInfo = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateData = {
+        $set: {
+          photo: updateInfo.photo,
+          name: updateInfo.name,
+          brandName: updateInfo.brandName,
+          type: updateInfo.type,
+          price: updateInfo.price,
+          rating: updateInfo.rating,
+        },
+      };
+      const result = await productCollection.updateOne(filter, updateData);
+      res.send(result);
+    });
+
+    // update user with cart product
+    app.put("/cart/:email", async (req, res) => {
+      const activeUserEmail = req.params.email;
+      const data = req.body;
+      const filter = { email: activeUserEmail };
+      const option = { upsert: true };
+      const updateData = {
+        $push: {
+          productId: data.productIds,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateData, option);
+      res.send(result);
+    });
+
+    // update user cart product by deleting item
+    app.put("/cart/delete/:email", async (req, res) => {
+      const activeUserEmail = req.params.email;
+      const data = req.body;
+      console.log(data);
+      const filter = { email: activeUserEmail };
+      const option = { multi: false };
+      const updateData = {
+        $pull: {
+          productId: data.productIds,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateData, option);
+      res.send(result);
+    });
+
+    // add user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+      console.log(result);
+    });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
